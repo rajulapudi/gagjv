1cfe19ae3fc061b6ae761b81ef829cb6139ab13c
rajulapudi <rajulapudip@gmail.com>
2021-10-09 00:52:02 +0530

    completed-v1

--- public/js/custom.js
+++ public/js/custom.js
@@ -19,11 +19,11 @@ $(document).ready(collapseNavbar);
 
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
-    $('.navbar-toggle:visible').click();
-  }
-});
+// $('.navbar-collapse ul li a').click(function() {
+//   if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+//     $('.navbar-toggle:visible').click();
+//   }
+// });
 
 
 
